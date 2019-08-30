08fb01fbc3aa178283552f8632fd7e172cd91d2b
angganova2019 <angga24.opa@gmail.com>
2019-08-30 16:25:44 +0700

    commit 30082019

---
+++ src/config/firebase/index.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase/app';
+// import 'firebase/auth';
+// import 'firebase/firestore';
+
+var firebaseConfig = {
+    apiKey: "AIzaSyDl2CaH2SaycT5saFmIIQtavLeit9bnJSA",
+    authDomain: "simple-notes-firebases.firebaseapp.com",
+    databaseURL: "https://simple-notes-firebases.firebaseio.com",
+    projectId: "simple-notes-firebases",
+    storageBucket: "",
+    messagingSenderId: "1007308894631",
+    appId: "1:1007308894631:web:c0be085b2ca17a08"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
