94d3efc7b3ef79edbaa9043f6c74fd88d8550bee
angganova2019 <angga24.opa@gmail.com>
2019-08-31 18:28:48 +0700

    commit sabtu 31082019

--- src/containers/pages/Register/index.js
+++ src/containers/pages/Register/index.js
@@ -1,12 +1,41 @@
 import React, { Component } from 'react';
+import './Register.scss';
+import firebase from '../../../config/firebase';
 
 class Register extends Component {
+    state = {
+        email: '',
+        password: ''
+    }
+
+    handleChangeText = (e) => {
+        this.setState({
+            [e.target.id]: e.target.value
+        })
+    }
+
+    handleRegisterSubmit = () => {
+        const { email, password } = this.state;
+        console.log('before send: ', email, password);
+        firebase.auth().createUserWithEmailAndPassword(email, password).then(res => {
+            console.log('success: ', res);
+        }).catch(function (error) {
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            console.log(errorCode, errorMessage);
+        });
+    }
+
     render() {
         return (
-            <div>
-                <p>Register Page</p>
-                <button>Go to Login</button>
-                <button>Go to Dashboard</button>
+            <div className="auth-container">
+                <div className="auth-card">
+                    <p className="auth-title">Register Page</p>
+                    <input className="input" id="email" type="text" placeholder="Email" onChange={this.handleChangeText} />
+                    <input className="input" id="password" type="password" placeholder="Password" onChange={this.handleChangeText} />
+                    <button className="btn" onClick={this.handleRegisterSubmit}>Register</button>
+                    {/* <button>Go to Dashboard</button> */}
+                </div>
             </div>
         )
     }
